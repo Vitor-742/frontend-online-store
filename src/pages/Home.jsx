363bd60646d55ddf498327ac74d1f1b217e639b4
Giovanna <giovannaelizz@hotmail.com>
2022-01-19 17:56:23 -0300

    requisito 4 ok camilla e giovanna

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
+import { getCategories } from '../services/api';
 
 class Home extends React.Component {
   constructor() {
     super();
     this.state = {
       productsList: [],
+      categories: [],
     };
+    this.updateCategories = this.updateCategories.bind(this);
+  }
+
+  componentDidMount() {
+    this.updateCategories();
+  }
+
+  async updateCategories() {
+    const categories = await getCategories();
+    this.setState({ categories });
   }
 
   render() {
-    const { productsList } = this.state;
+    const { productsList, categories } = this.state;
     const initailMessage = (
       <span
         data-testid="home-initial-message"
@@ -17,9 +29,27 @@ class Home extends React.Component {
         Digite algum termo de pesquisa ou escolha uma categoria.
       </span>
     );
-    if (productsList.length === 0) return initailMessage;
+
     return (
-      <div />
+
+      <div>
+        <div className="fixMessege">
+          { productsList.length === 0 && initailMessage }
+        </div>
+        <div className="fixButton">
+          {categories.map(({ id, name }) => (
+            <button
+              type="button"
+              data-testid="category"
+              key={ id }
+            >
+
+              { name }
+
+            </button>
+          ))}
+        </div>
+      </div>
     );
   }
 }
